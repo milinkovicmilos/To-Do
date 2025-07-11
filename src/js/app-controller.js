2c330216b54e4f900e60c379cc1ecb4d87a1f39d
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-07-11 23:39:02 +0200

    Handling add projects; Reference to AppController in DomController

--- src/js/app-controller.js
+++ src/js/app-controller.js
@@ -1,5 +1,8 @@
 import { DomController } from "./dom-controller.js";
 import { DummyStorageAPI } from "./dummy-storage-api.js";
+import { ProjectsHandler } from "./projects-handler.js";
+import { DummyProjectGenerator } from "./dummy-project-generator.js";
+import { Project } from "./project.js";
 
 export class AppController {
     /**
@@ -13,9 +16,15 @@ export class AppController {
      */
     #storageWrapper;
 
+    /**
+     * @type {object} projectsHandler
+     */
+    #projectsHandler;
+
     constructor() {
-        this.#domController = new DomController();
+        this.#domController = new DomController(this);
         this.#storageWrapper = new DummyStorageAPI();
+        this.#projectsHandler = new ProjectsHandler();
     }
 
     initializePage() {
@@ -23,15 +32,24 @@ export class AppController {
         const projects = this.#storageWrapper.loadProjects();
 
         // Display those projects
-
         this.#domController.renderProjects(projects);
+
+        // Get those projects into the projects handler
+        projects.forEach((project) => this.#projectsHandler.addProject(project));
 
         // Notifications?
     }
 
     createProject() {
         // DOM
+        const dummyProjectGenerator = new DummyProjectGenerator();
+        const projectData = dummyProjectGenerator.getProject();
+
         // Projects Handler
+        const project = new Project(projectData.title, projectData.desc);
+        this.#projectsHandler.addProject(project);
+
         // ...
+
     }
 }
--- src/js/dom-controller.js
+++ src/js/dom-controller.js
@@ -1,6 +1,20 @@
+import { AppController } from "./app-controller.js";
 import { Project } from "./project.js";
 
 export class DomController {
+    /**
+     * The App Controller object that created this controller
+     * @param {object} appController
+     */
+    #appController;
+
+    constructor(appController) {
+        if (!appController instanceof AppController) {
+            throw new Error("Invalid app controller passed. Must be instance of AppContoller.");
+        }
+        this.#appController = appController;
+    }
+
     /**
      * Render the projects onto a page
      * @param {Array} projects - array of projects (Objects of type Project).
@@ -20,7 +34,7 @@ export class DomController {
 
     #checkIfProjectArray(array) {
         if (!Array.isArray(array)) {
-            throw new Error("Must pass object that is instance of Project.");
+            throw new Error("Must pass an array.");
         }
 
         if (array.length == 0) {
