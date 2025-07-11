9902febb9c440dd1ca3b4d5ba1beeb2c7de0aa21
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-07-11 23:19:55 +0200

    Mock storage - added ability to temporarily store projects

--- src/js/dummy-storage-api.js
+++ src/js/dummy-storage-api.js
@@ -1,11 +1,21 @@
 import { Project } from "./project.js";
 
 export class DummyStorageAPI {
+    #projects = [
+        new Project('Test Project one', 'Description of it'),
+        new Project('Test Project two', 'Seconds description of it'),
+        new Project('Test Project three', 'And the third'),
+    ];
+
     loadProjects() {
-        return [
-            new Project('Test Project one', 'Description of it'),
-            new Project('Test Project two', 'Seconds description of it'),
-            new Project('Test Project three', 'And the third'),
-        ];
-    };
+        return this.#projects;
+    }
+
+    storeProject(project) {
+        if (project instanceof Project) {
+            this.#projects.push(project);
+        } else {
+            throw new Error("Invalid object for storage.");
+        }
+    }
 }
