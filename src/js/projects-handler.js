6cff07fe8dc213a1d236c1fe15845406913d1750
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-07-11 23:03:48 +0200

    Refactored into class

--- src/js/projects-handler.js
+++ src/js/projects-handler.js
@@ -1,44 +1,42 @@
 import { Project } from "./project.js";
 
-export const projectsHandler = (function() {
-    let projects = [];
+export class ProjectsHandler {
+    #projects = [];
 
     /**
      * Fetches all the projects that handler is handling
      */
-    const getProjects = () => {
-        return projects;
+    getProjects() {
+        return this.#projects;
     };
 
     /**
      * Fetch a single project
      */
-    const getProject = (index) => {
-        if (typeof index != 'number' || index < 0 || index > projects.length - 1) {
+    getProject(index) {
+        if (typeof index != 'number' || index < 0 || index > this.#projects.length - 1) {
             throw new Error("Invalid project index passed.");
         }
-        return projects[index];
+        return this.#projects[index];
     };
 
     /**
      * @param {object} project - Project object (instance of Project) to add
      */
-    const addProject = (project) => {
+    addProject(project) {
         if (!project instanceof Project) {
             throw new Error("Invalid object passed. Object must be instance of Project class.");
         }
-        projects.push(project);
+        this.#projects.push(project);
     };
 
     /**
      * @param {number} index - Index of project to remove
      */
-    const removeProject = (index) => {
-        if (typeof index != 'number' || index < 0 || index > projects.length - 1) {
+    removeProject(index) {
+        if (typeof index != 'number' || index < 0 || index > this.#projects.length - 1) {
             throw new Error("Invalid project index passed.");
         }
-        projects.splice(index, 1);
+        this.#projects.splice(index, 1);
     };
-
-    return { getProjects, getProject, addProject, removeProject };
-})();
+}
