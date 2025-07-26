76857b52d8323403e8084aef945336e663335a29
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-07-26 20:41:14 +0200

    Responsive changes

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import "../css/styles.css";
 import "../css/responsive-md.css";
 import "../css/responsive-lg.css";
 import "../css/responsive-xlg.css";
+import "../css/responsive-xxlg.css";
 
 import { AppController } from "./app-controller.js";
 
